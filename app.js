4a3673250d873393b3eab3b171850a23060d6530
Erik Shandrowski <erik843@gmail.com>
2014-07-22 17:09:40 -0600

    Finished

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.post('/formsubmit', function(req, res) {
+	res.redirect('/success');
+})
+
+app.get('/success', function(req, res) {
+	res.render('success');
+})
+
+var server = app.listen(4994, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
